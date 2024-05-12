e8a53c0469d1a622e78e9ac63eb914367606ca74
houssam ghmari <51957086+houssam980@users.noreply.github.com>
2024-05-12 20:24:07 +0100

    db connection

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$db   = 'medeca_db';
+$user = 'root';
+$pass = '';
+
+$mysqli = new mysqli($host, $user, $pass, $db);
+
+if ($mysqli->connect_error) {
+    die("Connection failed: " . $mysqli->connect_error);
+}
+
+?>
